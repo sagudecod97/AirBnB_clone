0288fa7fc10bc9d4bf0e5c44ae55739c5c9f3a75
Santiago <sagudecod97@gmail.com>
2019-11-08 23:59:02 +0000

    Oscar-chan, Por favor hace Pull

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,6 +18,7 @@ class FileStorage:
     def new(self, obj):
         """Method New."""
         new_obj = obj.__dict__.copy()
+        new_obj["__class__"] = type(obj).__name__
         new_obj["created_at"] = datetime.isoformat(new_obj["created_at"])
         new_obj["updated_at"] = datetime.isoformat(new_obj["updated_at"])
         self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = new_obj
