798b0b74399a7949192854af2b084bd34a874233
Santiago <sagudecod97@gmail.com>
2019-11-08 00:17:58 +0000

    Created a lot of stuff Baby UwU Oscar-chan

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ from json import dumps, loads
 import csv
 import uuid
 from datetime import datetime
-
+from models import storage
 
 class BaseModel:
     """A representation of the base of our OOP hierarchy."""
@@ -15,6 +15,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        storage.new(self)
 
         args_len = len(args)
 
@@ -35,36 +36,13 @@ class BaseModel:
     def save(self):
         """ method save: Updates the time of the property Update_at"""
         self.updated_at = datetime.now()
+        self.updated_at = datetime.isoformat(self.updated_at) #  Cast the self.updated to the rigth format
+        storage.save()
 
     def to_dict(self):
         ret_dict = self.__dict__
         ret_dict["__class__"] = BaseModel.__name__
         ret_dict["created_at"] = str(datetime.isoformat(self.created_at))
         ret_dict["updated_at"] = str(datetime.isoformat(self.updated_at))
+
         return ret_dict
-
-def main():
-    my_model = BaseModel()
-    my_model.name = "Holberton"
-    my_model.my_number = 89
-    print(my_model.id)
-    print(my_model)
-    print(type(my_model.created_at))
-    print("--")
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-    print("--")
-    my_new_model = BaseModel(**my_model_json)
-    print(my_new_model.id)
-    print(my_new_model)
-    print(type(my_new_model.created_at))
-
-    print("--")
-    print(my_model is my_new_model)
-
-
-main()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,4 +8,28 @@ from datetime import datetime
 
 class FileStorage:
     """A representation of the base of our OOP hierarchy."""
-    pass
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        new_obj = obj.__dict__
+        new_obj["created_at"] = datetime.isoformat(new_obj["created_at"])
+        new_obj["updated_at"] = datetime.isoformat(new_obj["updated_at"])
+        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = new_obj
+
+    def save(self):
+        with open(self.__file_path, 'w', encoding="utf-8") as f:
+            new_obj = self.__objects
+            print(new_obj)
+            f.write(dumps(new_obj))
+
+    def reload(self):
+        try:
+            with open(self.__file_path, 'r', encoding="utf-8") as f:
+                objects = loads(f.read())
+                self.__objects = objects
+        except Exception:
+            pass
