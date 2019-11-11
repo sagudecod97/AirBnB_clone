c3609d9fd21faf0e91fcd97ca2e7937e08ccafd2
Santiago <sagudecod97@gmail.com>
2019-11-11 21:44:28 +0000

    LOL

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Module for HBNBCommand class."""
 import cmd
+import json
+import sys, inspect
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
@@ -26,24 +28,92 @@ class HBNBCommand(cmd.Cmd):
     # ------ Instance commands ------
 
     def do_create(self, arg):
+        arr_classes = inspect.getmembers(sys.modules[__name__], inspect.isclass)
         arg_split = arg.split(' ')
         if (arg_split[0] == ''):
             print("** class name missing **")
         else:
-            try:
-                instance = eval(arg_split[0] + "()")
-                print((instance.__dict__)["id"])
-                instance.save()
-            except Exception:
+            for k in arr_classes:
+                if arg_split[0] in k:
+                    instance = eval(arg_split[0] + "()")
+                    print((instance.__dict__)["id"])
+                    instance.save()
+                    flag = 0
+                    break
+                else:
+                    flag = 1
+            if flag:
                 print("** class doesn't exist **")
 
     def do_show(self, arg):
+        flag = 0
+        arr_classes = inspect.getmembers(sys.modules[__name__], inspect.isclass)
         arg_split = arg.split(' ')
         if (arg_split[0] == ''):
             print("** class name missing **")
         else:
-            try:
-                #with open(FileStorage.__path_file
+            for k in arr_classes:
+                if arg_split[0] in k:
+                    flag = 0
+                    break
+                else:
+                    flag = 1
+            if flag:
+                print("** class doesn't exist **")
+            elif flag == 0 and len(arg_split) == 1:
+                print("** instance id missing **")
+
+            if len(arg_split) > 1:
+                for item in storage.all():
+                    if arg_split[1] in item:
+                        flag = 0
+                        break
+                    else:
+                        flag = 1
+                if flag:
+                    print("** no instance found **")
+                else:
+                    for key,value in storage.all().items():
+                        if arg_split[1] in key:
+                            print(value)
+
+    def do_destroy(self, arg):
+        flag = 0
+        arr_classes = inspect.getmembers(sys.modules[__name__], inspect.isclass)
+        arg_split = arg.split(' ')
+        if (arg_split[0] == ''):
+            print("** class name missing **")
+        else:
+            for k in arr_classes:
+                if arg_split[0] in k:
+                    flag = 0
+                    break
+                else:
+                    flag = 1
+            if flag:
+                print("** class doesn't exist **")
+            elif flag == 0 and len(arg_split) == 1:
+                print("** instance id missing **")
+
+            if len(arg_split) > 1:
+                for item in storage.all():
+                    if arg_split[1] in item:
+                        flag = 0
+                        break
+                    else:
+                        flag = 1
+                if flag:
+                    print("** no instance found **")
+                else:
+                    copy_destroy = storage.all()
+                    flag = 0
+                    for key,value in copy_destroy.items():
+                        if arg_split[1] in key:
+                            flag = 1
+                    if flag:
+                        del copy_destroy[key]
+
+
 
 def main():
     HBNBCommand().cmdloop()
