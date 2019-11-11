7869073e76396ed639f117ae954b7f3a389f68fb
Santiago <sagudecod97@gmail.com>
2019-11-11 17:01:50 +0000

    Almost fixed 5

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ from json import dumps, loads
 import csv
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
@@ -16,7 +16,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        storage.new(self)
+        models.storage.new(self)
 
         args_len = len(args)
 
@@ -24,7 +24,9 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
-                elif key in ["created_at", "updated_at"]:
+                elif key == "created_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == "updated_at":
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 setattr(self, key, value)
 
@@ -36,8 +38,8 @@ class BaseModel:
     def save(self):
         """ method save: Updates the time of the property Update_at"""
         self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         ret_dict = self.__dict__
